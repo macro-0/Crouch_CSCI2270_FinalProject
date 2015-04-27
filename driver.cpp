8262459ec03b7f6d44456b2acabffafe14c8f6cf
macro-0 <madeleinehcrouch@gmail.com>
2015-04-27 14:50:42 -0600

    Update driver.cpp

--- driver.cpp
+++ driver.cpp
@@ -2,33 +2,25 @@
 #include <fstream>
 #include <vector>
 #include <sstream>  // stringstream
-#include <string.h>  // 
+#include <string.h>  // compare
 #include <algorithm>  // transform
 #include "BinarySearchTree.h"
+#include "RedBlackBST.h"
 
 // todo: search either by scientific name or by common name
 // todo: add functions to allow user to completely clear current tree
 // todo: typecheck/other solution to stop menu crash
 // todo: enable user to delete specific records of a sighting without deleting the node
 // todo: enable user to add multiple records at once, ex. more than one sighting of common lilac at different elevations and sites
-// todo: convert all of characters in common name to lower case, rather than just first
 
-/*
-using std::string;
-using std::cout;
-using std::endl;
-using std::cin;
-using std::ifstream;
-using std::vector;
-using std::stringstream;*/
 using namespace std;
 
 void printMenu();
 void readIn(BinarySearchTree *bst, char *filename);
+void readIn(RedBlackTree *rb, char *filename);
 
 int main(int argc, char* argv[]) {
-	BinarySearchTree *bst = new BinarySearchTree();
-	readIn(bst, argv[1]); 
+	
 	string tree_choice = "";
 	string str_choice = "";
 	int choice = -1;
@@ -47,10 +39,12 @@ int main(int argc, char* argv[]) {
 	
 	//bst->addDataNode(commonName, sciName, phenophase, elevation, siteID, date, count);
 	while (!quit) {
-		cout << "Please enter which type of tree you would like to build: (u)nbalanced, (r)ed-black, or (a)vl, or (q)uit: " << endl;
+		cout << "Please enter which type of tree you would like to build: (u)nbalanced or (r)ed-black, or (q)uit: " << endl;
 		getline(cin, tree_choice);
 		if (tree_choice == "u") {
 			cout << "You have chosen: unbalanced BST" << endl;
+			BinarySearchTree *bst = new BinarySearchTree();
+			readIn(bst, argv[1]); 
 			while (!bstQuit) {
 				printMenu();
 				getline(cin, str_choice);
@@ -97,10 +91,35 @@ int main(int argc, char* argv[]) {
 						break;
 				}
 			}
-		} //else if (tree_choice == "r") { 
+		} else if (tree_choice == "r") { 
+			cout << "You have chosen: red-black BST" << endl;
+			RedBlackTree *rb = new RedBlackTree();
+			readIn(rb, argv[1]); 
+			while (!rbBstQuit) {
+				printMenu();
+				getline(cin, str_choice);
+				choice = stoi(str_choice);
+				switch(choice) {
+					case 1:
+					rb->printTree();
+						break;
+					case 2:
+						break;
+					case 3:
+						break;
+					case 4:
+						break;
+					case 5:
+						rbBstQuit = true;
+						break;
+					default:
+						cout << "invalid input, please try again" << endl;
+						break;
+				}
+			}
+				
 		//} else if (tree_choice == "a") {
-		//}
-		else if (tree_choice == "q") {
+		} else if (tree_choice == "q") {
 			cout << "Goodbye!" << endl;
 			quit = true;
 		} else {
@@ -158,6 +177,55 @@ void readIn(BinarySearchTree *bst, char *filename) {
 								
 				//bst->addDataNode(commonName, sciName, phenophase, elevation, siteID, date, count);
 				bst->addDataNode(commonName, sciName, data[11], atoi(data[3].c_str()), atoi(data[0].c_str()), in_date, 0);
+				in_date = "";  // reset
+			}	
+		}
+		infile.close();
+		
+	} else {
+		cout << "Error opening infile, please check CLA" << endl;
+	}
+}
+
+void readIn(RedBlackBST *rb, char *filename) {
+	ifstream infile(filename);
+	cout << filename << endl;
+	
+	if (infile.is_open()) {
+		vector<string> attributes;  // pull attributes from first line
+		string line;
+		string attribute;
+		getline(infile, line);
+		stringstream ss(line);
+		while (getline(ss, attribute, ',')) {
+			attributes.push_back(attribute);  // vector of record types 
+		}
+		
+		string commonName = "";
+		string sciName = "";
+		string in_date = "";
+		
+		while (infile) {
+			vector<string> data;  // pull attributes from first line
+			getline(infile, line);
+			if (line != "") {  // last line from txt file is blank
+				stringstream ss(line);
+				string datum;
+				while (getline(ss, datum, ',')) {
+					data.push_back(datum);  // vector of animal/plant data
+				}
+				
+				commonName = data[8];
+				transform(commonName.begin(), commonName.end(), commonName.begin(), ::tolower);
+				
+				sciName = data[6] + " " + data[7];  // sciName = "genus" + " " + "species"
+				if (atoi(data[13].c_str()) < 10) {  // format date
+					in_date += "0";
+				}
+				in_date += data[13] + data[14] + data[12]; 
+								
+				//bst->addDataNode(commonName, sciName, phenophase, elevation, siteID, date, count);
+				rb->addDataNode(commonName, sciName, data[11], atoi(data[3].c_str()), atoi(data[0].c_str()), in_date, 0);
 				in_date = "";  // reset
 			}	
 		}
